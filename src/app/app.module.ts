b053b51b20440301ff778d659a8432de2138d721
AmrAhmedHussien <AmrAhmedHussien@outlook.com>
2022-02-20 04:56:22 +0200

    Teacher page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { InstructorCardComponent } from './instructor-card/instructor-card.component';
 import { CardContainerComponent } from './card-container/card-container.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { InstructorListComponent } from './instructor-list/instructor-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InstructorCardComponent,
     CardContainerComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    TestimonialsComponent,
+    InstructorListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/testimonials/testimonials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testimonials',
+  templateUrl: './testimonials.component.html',
+  styleUrls: ['./testimonials.component.css']
+})
+export class TestimonialsComponent implements OnInit {
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
