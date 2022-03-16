5a1bcbc5417c196782cf60c723bacff95d775929
menna <menaabdo076@gmail.com>
2022-03-16 12:17:49 +0200

    activiate payment method

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -3,6 +3,8 @@ import { course } from '../models/course.model';
 import { CourseserveService } from '../service/courseserve.service';
 import { ActivatedRoute } from '@angular/router';
 import { InstructorserviceService } from '../service/instructorservice.service';
+import { PaymentService } from '../service/payment.service';
+import { UserService } from '../service/user.service';
 @Component({
   selector: 'app-addcourse',
   templateUrl: './addcourse.component.html',
@@ -14,15 +16,30 @@ selectedfile?:any
 mess?:any
 course_img!:any
 course_id!:number
-  constructor(private courseserve:CourseserveService,private activeroute:ActivatedRoute,private instserve:InstructorserviceService) { }
+user!:any
+url!: string;
+fatoohrares!: any;
+  constructor(private userService: UserService, private payserve: PaymentService,private courseserve:CourseserveService,private activeroute:ActivatedRoute,private instserve:InstructorserviceService) { }
  id:number=(this.activeroute.snapshot.params['id']) as number;
   ngOnInit(): void {
+    this.userService.userlogin().subscribe((res) => {
+      this.user = res;})
   }
 add(){
+  this.payserve
+  .payment({
+    fname: this.user.fname,
+    phone: this.user.phone,
+    email: this.user.email,
+  })
+  .subscribe((res) => {
+    console.log(res);
+    this.fatoohrares = res;
+    this.url = this.fatoohrares.Data.InvoiceURL;
+    window.location.href=this.url
+  });
   
   this.course.instructor_id=this.id
- 
-
   console.log(this.course)
   this.courseserve.add(this.course).subscribe(res=>{console.log(res);
     this.mess=(res)
