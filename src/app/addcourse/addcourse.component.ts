1e17fe657d6124f0140475dae19ae6528a8ca764
menna <menaabdo076@gmail.com>
2022-03-16 19:08:02 +0200

    update addcourse

--- src/app/addcourse/addcourse.component.ts
+++ src/app/addcourse/addcourse.component.ts
@@ -22,33 +22,20 @@ fatoohrares!: any;
   constructor(private userService: UserService, private payserve: PaymentService,private courseserve:CourseserveService,private activeroute:ActivatedRoute,private instserve:InstructorserviceService) { }
  id:number=(this.activeroute.snapshot.params['id']) as number;
   ngOnInit(): void {
-    this.userService.userlogin().subscribe((res) => {
+    this.userService.userlogin().subscribe((res) => {console.log(res)
       this.user = res;})
   }
 add(){
-  this.payserve
-.payment({
-    fname: this.user.fname,
-    phone: this.user.phone,
-    email: this.user.email,
-  })
-  .subscribe((res) => {
-    console.log(res);
-    this.fatoohrares = res;
-    this.url = this.fatoohrares.Data.InvoiceURL;
-    window.location.href=this.url
-  });
-  this.payserve.status().subscribe(res=>{
-    if(res==1){
-  this.course.instructor_id=this.id
+ 
+  this.course.instructor_id=this.user.id
   console.log(this.course)
   this.courseserve.add(this.course).subscribe(res=>{console.log(res);
     this.mess=(res)
     this.uploadimg()
   }
     )}
- } )
-  }
+ 
+  
 selectedFile(e:any){
   this.selectedfile= <File> e.target.files[0]
     console.log(this.selectedfile)
