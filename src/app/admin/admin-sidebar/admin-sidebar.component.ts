fb67d3217b98a99bac4a6ae2a4acaaff1c5fdda3
AhmedHafez7-Eng <ahmedhafezoffic@gmail.com>
2022-03-20 00:11:03 +0200

    Fix Forms Validation

--- src/app/admin/admin-sidebar/admin-sidebar.component.ts
+++ src/app/admin/admin-sidebar/admin-sidebar.component.ts
@@ -1,30 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/service/user.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-admin-sidebar',
   templateUrl: './admin-sidebar.component.html',
-  styleUrls: ['./admin-sidebar.component.css']
+  styleUrls: ['./admin-sidebar.component.css'],
 })
 export class AdminSidebarComponent implements OnInit {
+  id!: number;
+  user!: any;
+  loggedIn: any;
 
-  id!:number
-  user!:any
-  loggedIn:any;
-
-  constructor(private userService: UserService) { }
+  constructor(private userService: UserService, private activeroute: Router) {}
 
   ngOnInit(): void {
-
-    { this.userService.isUserLoggedIn().subscribe(
-      ( status) => {this.loggedIn = status
-       console.log('isLogged', this.loggedIn);
-      this.userService.userlogin().subscribe(res=>this.user=res)
-     }
-
-     );}
+    {
+      this.userService.isUserLoggedIn().subscribe((status) => {
+        this.loggedIn = status;
+        console.log('isLogged', this.loggedIn);
+        this.userService.userlogin().subscribe((res) => (this.user = res));
+      });
+    }
   }
   logout(): void {
     this.userService.logout();
-    //this.activeroute.navigate(['/login'])
+    // this.activeroute.navigate(['/login']);
+    // window.location.reload();
   }
 }
