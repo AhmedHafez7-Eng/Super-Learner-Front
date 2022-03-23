30183775d7c42b8214ba9021bda7306f044f1b06
AhmedHafez7-Eng <ahmedhafezoffic@gmail.com>
2022-03-23 18:51:01 +0200

    Show Instructor name on courses

--- src/app/course-view/course-view.component.ts
+++ src/app/course-view/course-view.component.ts
@@ -4,31 +4,27 @@ import { UserService } from '../service/user.service';
 @Component({
   selector: 'app-course-view',
   templateUrl: './course-view.component.html',
-  styleUrls: ['./course-view.component.css']
+  styleUrls: ['./course-view.component.css'],
 })
-
 export class CourseViewComponent implements OnInit {
-  user!:any
-  action!:string
+  user!: any;
+  action!: string;
   @Input()
-  course!:course;
-  role!:string
-  constructor(private userService: UserService) { }
-  
-  ngOnInit(): void {
-    this.userService.userlogin().subscribe((res)=>{this.user=res
-      if(this.user.role=='instructor'){
-      this.action='edit'
-      this.role='instructor'
-    
-    }
-      else {
-        this.action='show your progress'
-        this.role='student'
-      }
-  })
-}
-  
-  
+  // course!: course;
+  course!: any;
+  role!: string;
+  constructor(private userService: UserService) {}
 
+  ngOnInit(): void {
+    this.userService.userlogin().subscribe((res) => {
+      this.user = res;
+      if (this.user.role == 'instructor') {
+        this.action = 'edit';
+        this.role = 'instructor';
+      } else {
+        this.action = 'show your progress';
+        this.role = 'student';
+      }
+    });
+  }
 }
